981cf7dc3d0c6e21faa2c71f2a80bed24396cf03
aswin7469 <aswinas@pixysos.com>
2020-09-09 08:44:41 +0000

    msm8996-common: remove deprecated ims-ext-common
    Signed-off-by: aswin7469 <aswinas@pixysos.com>

--- msm8937.mk
+++ msm8937.mk
@@ -335,8 +335,6 @@ PRODUCT_PACKAGES += \
 # Telephony
 PRODUCT_PACKAGES += \
     telephony-ext \
-    ims-ext-common_system \
-    ims_ext_common.xml \
     qti-telephony-hidl-wrapper \
     qti_telephony_hidl_wrapper.xml \
     qti-telephony-utils \
